1965f33821ade173198fea8df5682309c93eb42d
artsemy <artemiy.konoplyanik@gmail.com>
2019-08-07 22:07:09 +0300

    add functions

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,5 +9,9 @@ public class Main {
         myClass.function3(3,3);
         myClass.function4(222.333);
         myClass.function5(10983);
+        myClass.function6(2, 3);
+        myClass.function7(30, 90);
+        myClass.function8(3,5, 4, 6);
+        myClass.function9(1,1,2,2,6,6);
     }
 }
--- src/com/company/MyClass.java
+++ src/com/company/MyClass.java
@@ -1,37 +1,74 @@
 package com.company;
 
 import java.time.LocalTime;
-import java.time.format.DateTimeFormatter;
-
 import static java.lang.Math.*;
 
-public class MyClass {
+class MyClass {
 
-    public void function1(int a, int b, int c){
+    void function1(int a, int b, int c){
         double z = ((a - 3) * b / 2 ) + c;
         System.out.println("result: " + z);
     }
 
-    public void function2(int a, int b, int c){
+    void function2(int a, int b, int c){
         double res = (b + pow(b*b + 4*a*c, 0.5))/(2*a) - pow(a, 3)*c + pow(b, -2);
         System.out.println("result: " + res);
     }
 
-    public void function3(int x, int y){
+    void function3(int x, int y){
         double res = (sin(x) + cos(y)) / (cos(x) - sin(y)) * tan(x*y);
         System.out.println("result: " + res);
     }
 
-    public void function4(double d){
+    void function4(double d){
         int i = (int) (d*1000);
         int fractionalPart = i % 1000;
         int integerPart =  i / 1000;
-        double res = integerPart + fractionalPart / 1000.;
+        double res = fractionalPart + integerPart / 1000.;
         System.out.println("result: " + res);
     }
 
-    public void function5(long T){
+    void function5(long T){
         LocalTime time = LocalTime.ofSecondOfDay(T);
-        System.out.format("%tHч %tMмин %tSс", time, time, time);
+        System.out.format("%tHч %tMмин %tSс%n", time, time, time);
     }
+
+    void function6(double x, double y){
+        if(y <= 0 && y >= -3 && x >= -4 && x <= 4){
+            System.out.println("true");
+        }else if(y >= 0 && y <= 4 && x >= -2 && x <= 2){
+            System.out.println("true");
+        } else{
+            System.out.println("false");
+        }
+    }
+
+    void function7(int angle1, int angle2){
+        int angle3 = 180 - angle1 - angle2;
+        if(angle1 > 0 && angle2 > 0 && angle3 > 0){
+            System.out.println("this is triangle");
+            if (angle1 == 90 || angle2 == 90 || angle3 == 90){
+                System.out.println("this is right-angled triangle");
+            } else {
+                System.out.println("this is not right-angled triangle");
+            }
+        } else {
+            System.out.println("this is not triangle");
+        }
+    }
+
+    void function8(int a, int b, int c, int d){
+        System.out.println(max(min(a, b), min(c, d)));
+    }
+
+    void function9(int x1, int y1, int x2, int y2, int x3, int y3){
+        double x = (x3 - x1) / (x2 - x1);
+        double y = (y3 - y1) / (y2 - y1);
+        if(x == y){
+            System.out.println("on line");
+        } else {
+            System.out.println("not on line");
+        }
+    }
+
 }
