dc36ea37a30ac60aabd1fc69f240beb32b9838c3
artsemy <artemiy.konoplyanik@gmail.com>
2019-08-08 15:12:56 +0300

    add functions finish

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,15 +3,31 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        MyClass myClass = new MyClass();
-        myClass.function1(1, 1, 1);
-        myClass.function2(1, 1, 1);
-        myClass.function3(3,3);
-        myClass.function4(222.333);
-        myClass.function5(10983);
-        myClass.function6(2, 3);
-        myClass.function7(30, 90);
-        myClass.function8(3,5, 4, 6);
-        myClass.function9(1,1,2,2,6,6);
+//        MyClass myClass = new MyClass();
+//        myClass.function1(1, 1, 1);
+//        myClass.function2(1, 1, 1);
+//        myClass.function3(3,3);
+//        myClass.function4(222.333);
+//        myClass.function5(10983);
+//        myClass.function6(2, 3);
+//        myClass.function7(30, 90);
+//        myClass.function8(3,5, 4, 6);
+//        myClass.function9(1,1,3,3,2,2);
+//        myClass.function10(6, 3, 2);
+//        myClass.function11(4);
+//        myClass.function12();
+//        myClass.function13(-5, 5, 1);
+//        myClass.function14();
+//        myClass.function15();
+//        myClass.function16(0.000000000001);
+//        myClass.function17();
+//        myClass.function18();
+//        myClass.function19(1239, 1123345678);
+
+//        try {
+//            System.in.close();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
     }
 }
--- src/com/company/MyClass.java
+++ src/com/company/MyClass.java
@@ -1,6 +1,9 @@
 package com.company;
 
+import java.io.*;
 import java.time.LocalTime;
+import java.util.Scanner;
+
 import static java.lang.Math.*;
 
 class MyClass {
@@ -62,12 +65,157 @@ class MyClass {
     }
 
     void function9(int x1, int y1, int x2, int y2, int x3, int y3){
-        double x = (x3 - x1) / (x2 - x1);
-        double y = (y3 - y1) / (y2 - y1);
+        double x = (double) (x3 - x1) / (x2 - x1);
+        double y = (double) (y3 - y1) / (y2 - y1);
         if(x == y){
             System.out.println("on line");
         } else {
             System.out.println("not on line");
+        }
+    }
+
+    void function10(int x, int y, int z){
+        int A = 2;
+        int B = 4;
+        if(checkCondition(A, B, x, y)){
+            System.out.println("yes, it can");
+        } else if(checkCondition(A, B, x, z)){
+            System.out.println("yes, it can");
+        } else if(checkCondition(A, B, y, z)){
+            System.out.println("yes, it can");
+        } else {
+            System.out.println("no, it can't");
+        }
+    }
+
+    private boolean checkCondition(int i1, int i2, int j1, int j2){
+        double d1 = (double) max(i1, i2) / min(i1, i2);
+        double d2 = (double) max(j1, j2) / min(j1, j2);
+        int area1 = i1 * i2;
+        int area2 = j1 * j2;
+        return (area1 >= area2) && (d1 >= d2);
+    }
+
+    void function11(double x){
+        double res;
+        if(x <= 3){
+            res = x * x - 3 * x + 9;
+        } else {
+            res = 1 / (pow(x, 3) + 6);
+        }
+        System.out.println("f= " + res);
+    }
+
+    void function12(){
+        System.out.println("insert number");
+        InputStream inputStream = System.in;
+        Reader inputStreamReader = new InputStreamReader(inputStream);
+        BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+        int n = 1;
+        try {
+            n = Integer.parseInt(bufferedReader.readLine());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        int res1 = (1 + n) * n / 2;
+        int res2 = 0;
+        for (int i = 1; i <= n; i++) {
+            res2 += i;
+        }
+        System.out.println(res1 + " = " + res2);
+//        try {
+//            inputStream.close();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+    }
+
+    void function13(int a, int b, double h){
+        double current = a;
+        while (current <= b){
+            if(current > 2){
+                System.out.print(current + " ");
+            } else {
+                System.out.print((-current) + " ");
+            }
+            current += h;
+        }
+    }
+
+    void function14(){
+        int res = 0;
+        for (int i = 1; i < 101; i++) {
+            res += i*i;
+        }
+        System.out.println("sum = " + res);
+    }
+
+    void function15(){
+        int res = 1;
+        for (int i = 1; i < 201; i++) {
+            res *= pow(i, 2);
+        }
+        System.out.println("multi = " + res);
+    }
+
+    void function16(double e){
+        double sum = 0;
+        double elem = mathFormula(0);
+        for (int i = 1; Double.compare(elem, e) >= 0; i++) {
+            sum += elem;
+            elem = mathFormula(i);
+        }
+        System.out.println("sum = " + sum);
+    }
+
+    private double mathFormula(int n){
+        return 1 / (pow(2, n)) + 1 / (pow(3, n));
+    }
+
+    void function17(){
+        for (int i = 32; i < 127; i++) {
+            System.out.println(((char) i) + " " + i);
+        }
+    }
+
+    void function18(){
+        double m = Double.parseDouble(getInputNumber());
+        double n = Double.parseDouble(getInputNumber());
+        int k = (int) ceil(m);
+        int end = (int) ceil(n);
+        while (k <= end){
+            findDividers(k);
+            k++;
+        }
+    }
+
+    private String getInputNumber(){
+        System.out.println("input number: ");
+        Scanner scanner = new Scanner(System.in);
+        String res = scanner.nextLine();
+//        scanner.close();
+        return res;
+    }
+
+    private void findDividers(int number){
+        System.out.print("divider for " + number + " is: ");
+        for (int i = 2; i < number; i++) {
+            if(number%i == 0){
+                System.out.print(i + " ");
+            }
+        }
+        System.out.println();
+    }
+
+    void function19(int a, int b){
+        String str1 = String.valueOf(a);
+        String str2 = String.valueOf(b);
+        for (int i = 0; i < 10; i++) {
+            boolean b1 = str1.contains(String.valueOf(i));
+            boolean b2 = str2.contains(String.valueOf(i));
+            if(b1 && b2){
+                System.out.print(i + " ");
+            }
         }
     }
 
